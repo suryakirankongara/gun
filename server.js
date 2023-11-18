932f1d1c61c61d6013d0a910586058805fed3193
suryakirankongara <82138478+suryakirankongara@users.noreply.github.com>
2023-11-18 10:44:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
-const express = require('express');
 const http = require('http');
 const Gun = require('gun');
 
-const app = express();
-const server = http.createServer(app);
-
-// Gun setup
-const gun = new Gun({ web: server });
-
-// Middleware to handle Gun requests
-app.use(gun.server);
-
-// Serve static files (e.g., your HTML, CSS, and JS)
-app.use(express.static('public'));
-
-const port = process.env.PORT || 8765;
-
-server.listen(port, () => {
-  console.log(`Gun server is running on port ${port}`);
-});
+const server = http.createServer(Gun.serve).listen(process.env.PORT || 8765);
 
 // Keep the application alive by sending a dummy request every 5 minutes
 setInterval(() => {
-  http.get('http://dechat.onrender.com');
+  http.get('http://gun.onrender.com');
 }, 300000); // 5 minutes
